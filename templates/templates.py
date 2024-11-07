a079ac4f99a496a41989bfebdee99daf09fe93cf
bia-pequeno <93928658+bia-pequeno@users.noreply.github.com>
2024-11-06 22:33:29 -0300

    up

---
+++ templates/templates.py
@@ -0,0 +1,40 @@
+import sys, os
+sys.path.append(os.path.abspath(os.curdir))
+from model.password import Password
+from view.password_views import FernetHasher
+
+
+action = input('Digite 1 salvar uma nova senha ou 2 para ver uma determinada senha: ')
+match action:
+    case '1':
+        if len(Password.get() or []) == 0:
+            key, path = FernetHasher.create_key(archive=True)
+            print('Sua chave foi criada, salve-a com cuidado caso a perca nao poderá recuperar suas senhas.') 
+            print(f'Chave:{key.decode("utf-8")}')
+            if path:
+                print('Chave salva no arquivo, lembre-se de remover o arquivo após o transferir de local')
+                print(f'Caminho:{path}')
+        else:
+            key = input('Digite sua chave usada para criptografia, use sempre a mesma chave: ')
+        domain = input('Domínio: ')
+        password = input('Digite a senha: ')
+        fernet = FernetHasher(key)
+        p1 = Password(domain=domain, password=fernet.encrypt(password).decode('utf-8'))
+        p1.save()
+
+    case '2':
+        domain = input('Domínio: ')
+        key = input('Key: ')
+        fernet = FernetHasher(key)
+        data = Password.get()
+
+        found_password = None
+        
+        for i in data:
+            if domain in i['domain']:
+                found_password = fernet.decrypt(i['password'])
+                break
+        if found_password:
+            print(f'Sua senha: {found_password}')
+        else:
+            print('Nenhuma senha encontrada para esse domínio.')
